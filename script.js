a9b2b1c45719868f8dce05f48da03118495495e5
Eugene Edwards <eedwards@su.edu>
2019-11-26 22:29:26 -0500

    Created timer div and container

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 //Main container fade in
-$(document).ready(function(){
-      $("#main-container").fadeIn(2000);
-  });
+$("#welcome-container").fadeIn(2000);
+
+var startButton = document.getElementById("btnStartQuiz");
+
+startButton.addEventListener("click", function(){
+    $("#welcome-container").fadeOut(100);
+    $("#quiz-container").fadeIn(2000);
+    $("#timer-container").fadeIn(2000);
+
+
+
+
+});
+ 
